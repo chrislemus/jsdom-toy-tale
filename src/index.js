8cb6503e3a09fe62b6b53170dd1d52e8fa461412
chrislemus <icrislemus@gmail.com>
2021-03-16 13:25:23 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ let addToy = false;
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
+  const newToyForm = document.querySelector('.add-toy-form')
+  const toyCollectionWrapper = document.querySelector('#toy-collection')
   addBtn.addEventListener("click", () => {
     // hide & seek with the form
     addToy = !addToy;
@@ -12,4 +14,37 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+  fetch('http://localhost:3000/toys')
+  .then(res => res.json())
+  .then(toys => {toyCollectionWrapper.innerHTML += toys.map(toy => {
+      return (
+        `<div class="card">
+          <h2>${toy.name}</h2>
+          <img src=${toy.image} class="toy-avatar" />
+          <p>${toy.likes} Likes </p>
+          <button class="like-btn">Like <3</button>
+        </div>`
+      )
+    }).join('')
+  })
+
+  
+
+  newToyForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const newToyInfo = Object.fromEntries(new FormData(e.target).entries());
+    const url = 'http://localhost:3000/toys'
+    const config = {
+      method: 'post',
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: JSON.stringify({...newToyInfo, likes: 0}) 
+    }
+    fetch(url, config)  
+  })
+
+
 });
